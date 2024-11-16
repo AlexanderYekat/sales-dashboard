81affa9c3318c8f649fae4dd378fa273f96839bc
AlexanderYekat <47818494+AlexanderYekat@users.noreply.github.com>
2024-11-17 02:16:36 +0500

    авторизация3

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // ваши настройки
+  // Отключаем все аналитики и метрики Vercel
+  vercelAnalytics: false,
+  speedInsights: false,
+  // Отключаем загрузку внешних скриптов Vercel
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "script-src 'self';"
+          }
+        ]
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
